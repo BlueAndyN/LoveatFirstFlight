8a7297420415afb1fad8809b268ebdf8be848832
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-26 21:29:01 -0600

    been, planned, review seed data added

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,6 +2,9 @@ const sequelize = require('../config/connection');
 const { User, Been, Planned, Review } = require('../models');
 
 const userData = require('./userData.json');
+const beenData = require('./beenData.json');
+const plannedData = require('./plannedData.json');
+const reviewData = require('./reviewData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -10,6 +13,10 @@ const seedDatabase = async () => {
     individualHook: true,
     returning: true,
   });
+
+  const been = await Been.bulkCreate(beenData, { returning: true });
+  const planned = await Planned.bulkCreate(plannedData, { returning: true });
+  const review = await Review.bulkCreate(reviewData, { returning: true });
 
   process.exit(0);
 };
