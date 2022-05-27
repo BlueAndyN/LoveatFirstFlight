9ee5653196d09ae58c949903284926026de8b13c
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-26 20:11:46 -0600

    Been, Planned, Review added to model index

--- models/index.js
+++ models/index.js
@@ -1,3 +1,28 @@
 const User = require('./User');
+const Been = require('./Been');
+const Planned = require('./Planned');
+const Review = require('./Review');
 
-module.exports = { User };
+User.hasMany(Been, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+User.hasMany(Planned, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+Been.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Been.hasMany(Review, {
+  foreignKey: 'been_id',
+});
+
+Planned.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+module.exports = { User, Been, Planned, Review };
