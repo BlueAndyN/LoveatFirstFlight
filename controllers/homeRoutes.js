cf9c5e50254742550df917373f6cad2d81796edf
Marcus Herrera <maherreraiv1987@gmail.com>
2022-05-31 19:31:28 -0600

    added been/planned cards

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,15 +5,15 @@ const withAuth = require('../utils/auth');
 // Homepage
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      include: [{ model: User }],
-    });
+    // const postData = await Post.findAll({
+    //   include: [{ model: User }],
+    // });
 
-    const posts = postData.map((post) => post.get({ plain: true }));
+    // const posts = postData.map((post) => post.get({ plain: true }));
 
     res.render('homepage', {
-      posts,
-      logged_in: req.session.logged_in,
+      // posts,
+      // logged_in: req.session.logged_in,
     });
   } catch (error) {
     res.status(500).json(error);
