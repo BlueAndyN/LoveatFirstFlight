4d12e3d37919b07bf58804af1c3feb2e2ea00e7c
Marcus Herrera <maherreraiv1987@gmail.com>
2022-06-06 18:12:03 -0600

    removed commented code

--- public/js/test.js
+++ public/js/test.js
@@ -712,37 +712,13 @@ document.getElementById('dc').addEventListener('click', function () {
   }
 });
 
-// const states = wantBtn.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[1].childNodes[3].childNodes[3];
-// console.log(states);
-
-// const beenBtn = document.querySelector('.btn1');
-// const wantBtn = document.querySelector('.btn2');
-
-// const state = document.querySelector('#al')
-// console.log(state);
-
-// wantBtn.addEventListener('click', function () {
-//   state.setAttribute('class', 'want')
-// })
-
-// const beenBtn1 = document.querySelector('.btn3');
-// const wantBtn1 = document.querySelector('.btn4');
-
-// const state1 = document.querySelector('#ak')
-// console.log(state);
-
-// wantBtn1.addEventListener('click', function () {
-//   state1.setAttribute('class', 'want')
-// })
 const beenList = document.querySelector('.been-list')
 const plannedList = document.querySelector('.planned-list')
 
 const outermodal = document.querySelector('.outermodal');
-// console.log(outermodal);
 
 outermodal.addEventListener('click', function (event) {
   let element=event.target
-  // console.log(element);
   if (element.matches("button")===true) {
     if (event.target.textContent==="Been To"){
       const elementsvg = document.getElementById(event.target.name)
@@ -753,7 +729,6 @@ outermodal.addEventListener('click', function (event) {
     beenName.setAttribute('class', 'list-group-item')
     beenName.appendChild(stateName)
     beenList.appendChild(beenName)
-    // element.setAttribute("data-dismiss", "modal")
     element.parentNode.parentNode.parentNode.parentNode.style.display = 'none'
 
     const state_name = elementsvg.textContent.trim();
@@ -797,10 +772,5 @@ outermodal.addEventListener('click', function (event) {
       }
   }
     }
-    // console.log(event.target)
-    // console.log(event.target.name)
-    // const elementsvg = document.getElementById(event.target.name)
-    // elementsvg.classList.add("want")
-    // console.log(event.target.textContent)
   }
 })
